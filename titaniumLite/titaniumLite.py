80bb7828b047c6f5653731d46500e27d627b09b7
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-01-02 19:23:01 +0530

    Select Command returns Panda Dataframe

--- titaniumLite/titaniumLite.py
+++ titaniumLite/titaniumLite.py
@@ -1,30 +1,62 @@
+from io import StringIO
 import socket
 import pandas as pd
+import re
 
 class titaniumLite:
 	
 	def __init__(self, host = '127.0.0.1', port = 8080):
 		self.s = socket.socket()         
-		
 		self.host = host
 		self.port = port               
-		 
+		# self.s.bind((self.host,self.port))	
 		self.s.connect((self.host, self.port))
+		
 
 	def __del__(self):
 		self.s.close()
 
 	def execute(self, query):
+
+		#-----------------------------------------
+		dfNeeded=False
+		if(bool(re.match('select',query,re.I))): #check if query is of type 'select', case insensitive.
+			dfNeeded = True
+		#-----------------------------------------
+		
 		self.s.send(query.encode())
-		return self.__dataframe(self.s.recv(2024))
-	
-	def __dataframe(self, output):
+		temp = self.__dataframe(self.s.recv(2024),dfNeeded)
+		return temp
+		
+	def __dataframe(self, output, dfNeeded=False):
 		table = output
-		return table.decode()
+		tableCopy = table.decode()
+		
+		if not dfNeeded: #Non 'select' type queries
+			return tableCopy
+		
+		else: #'select' type query, return dataframe
+			charsToReplace = ['+','-']
+			for char in charsToReplace:
+				tableCopy = tableCopy.replace(char,'')
+				
+			tableCopy = tableCopy.split('\n\n')
+
+			for i in range(0,len(tableCopy)):
+				tableCopy[i] = tableCopy[i].replace('|','')
+				tableCopy[i] = ','.join(tableCopy[i].split())
+
+			res = '\n'.join(tableCopy)
+			test = StringIO(res)
+			df = pd.read_csv(test,sep=',')
+
+			return df
+
+
 
 if __name__ == '__main__':
 	t = titaniumLite()
-	print(t.execute('use database db1;'))
+	t.execute('use database db1;')
 	print(t.execute('select * from stats;'))
+	# t.execute('use database db1;')
 	del t
-	
